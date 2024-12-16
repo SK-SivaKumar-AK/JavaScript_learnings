243c553f7a0ca89856f159e7d00132d3e8cac2a6
SivaKumarSK <sk.sivakumar.ak@gmail.com>
2024-12-16 15:25:33 +0530

    Chapter-4
    Number concept and methods are in another sheet.

---
+++ Chapter_4/script.js
@@ -0,0 +1,18 @@
+myVariable = "45";
+myNumber = 45;
+myFloat = 45.2;
+
+console.log(myNumber);
+console.log(myFloat);
+console.log(myFloat === myNumber);
+console.log(Number(myVariable));
+console.log(Number("Siva"));
+console.log(Number(true));
+
+// Number methods this is example
+console.log(Number.isInteger(45.2));
+
+console.log(Number.isNaN("45"));
+
+console.log(isNaN("siva"));
+
