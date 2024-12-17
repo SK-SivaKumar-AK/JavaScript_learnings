7d581de48fd4219ec30c9eb4dc612b54eb7b06f4
SivaKumarSK <sk.sivakumar.ak@gmail.com>
2024-12-17 10:00:58 +0530

    Chapter-8
    Switch statements

---
+++ Chapter_8/script.js
@@ -0,0 +1,17 @@
+//Switch statement
+let condition = 2;
+let result;
+switch(condition){
+    case 1:
+        result = `${condition}`;
+        break;
+    case 2:
+        result = `${condition}`;
+        break;
+    case 3:
+        result = `${condition}`;
+        break;
+    default :
+        result = `${condition}`;
+}
+console.log(result);
